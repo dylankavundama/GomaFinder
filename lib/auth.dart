c7e1ad0b85572e388a82b41502c733d0c30c8641
Dylankavvundama <85756267+dylankavundama@users.noreply.github.com>
2024-03-04 17:08:29 +0100

    ssss

---
+++ lib/auth.dart
@@ -0,0 +1,117 @@
+import 'dart:io';
+import 'package:collection/collection.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth_example/main.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+const withSilentVerificationSMSMFA = true;
+
+class ScaffoldSnackbar {
+  ScaffoldSnackbar(this._context);
+
+  factory ScaffoldSnackbar.of(BuildContext context) {
+    return ScaffoldSnackbar(context);
+  }
+
+  final BuildContext _context;
+}
+
+class AuthGate extends StatefulWidget {
+  const AuthGate({Key? key}) : super(key: key);
+  static String? appleAuthorizationCode;
+  @override
+  State<StatefulWidget> createState() => _AuthGateState();
+}
+
+class _AuthGateState extends State<AuthGate> {
+
+  String error = '';
+  String verificationId = '';
+
+  bool isLoading = false;
+
+  void setIsLoading() {
+    setState(() {
+      isLoading = !isLoading;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (withSilentVerificationSMSMFA && !kIsWeb) {
+      FirebaseMessaging messaging = FirebaseMessaging.instance;
+      messaging.requestPermission();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: FocusScope.of(context).unfocus,
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: Text(''),
+              onPressed: () => _handleMultiFactorException(
+                _signInWithGoogle,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _handleMultiFactorException(
+    Future<void> Function() authFunction,
+  ) async {
+    setIsLoading();
+    try {
+      await authFunction();
+    } on FirebaseAuthMultiFactorException catch (e) {
+      setState(() {
+        error = '${e.message}';
+      });
+      final firstTotpHint = e.resolver.hints
+          .firstWhereOrNull((element) => element is TotpMultiFactorInfo);
+      if (firstTotpHint != null) {
+        return;
+      }
+
+      final firstPhoneHint = e.resolver.hints
+          .firstWhereOrNull((element) => element is PhoneMultiFactorInfo);
+
+      if (firstPhoneHint is! PhoneMultiFactorInfo) {
+        return;
+      }
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        error = '${e.message}';
+      });
+    } catch (e) {
+      setState(() {
+        error = '$e';
+      });
+    }
+    setIsLoading();
+  }
+
+  Future<void> _signInWithGoogle() async {
+    final googleUser = await GoogleSignIn().signIn();
+    final googleAuth = await googleUser?.authentication;
+    if (googleAuth != null) {
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      // await auth.signInWithCredential(credential);
+    }
+  }
+}
