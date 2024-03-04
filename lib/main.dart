09fd4a67a93f008080d72e1e1fe394692de2763f
Dylankavvundama <85756267+dylankavundama@users.noreply.github.com>
2024-03-04 18:39:42 +0100

    ,mkdfkf

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'onboarding_screen.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Future.delayed(Duration(seconds: 10));
+  FlutterNativeSplash.remove();
+
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: OnboardingScreen(),
     );
   }
-
-  
 }
