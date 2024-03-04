4ec4f4881a9c60f0b239da9421251cd01ccc5aae
Dylankavvundama <85756267+dylankavundama@users.noreply.github.com>
2024-03-04 17:14:24 +0100

    jkj

--- lib/onboarding_screen.dart
+++ lib/onboarding_screen.dart
@@ -223,3 +223,21 @@ List<OnboardingContents> contents = [
         "Take control of notifications, collaborate live or on your own time.",
   ),
 ];
+
+
+
+class SizeConfig {
+  static MediaQueryData? _mediaQueryData;
+  static double? screenW;
+  static double? screenH;
+  static double? blockH;
+  static double? blockV;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenW = _mediaQueryData!.size.width;
+    screenH = _mediaQueryData!.size.height;
+    blockH = screenW! / 100;
+    blockV = screenH! / 100;
+  }
+}
