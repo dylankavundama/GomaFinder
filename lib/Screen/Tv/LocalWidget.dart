eee773bdab044c33b28b777e342e38619e67f282
Dylankavvundama <85756267+dylankavundama@users.noreply.github.com>
2024-03-22 21:40:59 +0100

    xxxxx

--- lib/Screen/Tv/LocalWidget.dart
+++ lib/Screen/Tv/LocalWidget.dart
@@ -18,59 +18,11 @@ class LocalWdget extends StatelessWidget {
     ThemeData theme = Theme.of(context);
     return Padding(
       padding: const EdgeInsets.all(6),
-      child: Container(
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(
-            Radius.circular(6),
-          ),
-        ),
-        width: screenWidth,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-//add
-            Container(
-              color: Colors.black,
-              child: Stack(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(6.0),
-                    // child: Image.network(
-                    //   widget.image,
-                    //   fit: BoxFit.cover,
-
-                    // ),
-
-                    child: FadeInImage.assetNetwork(
-                        height: screenHeight * 0.3,
-                        width: screenWidth,
-                        placeholder: 'assets/bb.png',
-                        image: imageLocal
-                        //fit: BoxFit.cover,
-                        ),
-                  ),
-                  Positioned(
-                    top: screenHeight * 0.2,
-                    width: screenWidth / 2,
-                    child: Container(
-                      width: screenWidth,
-                      color: Color.fromARGB(221, 191, 187, 187),
-                      child: Center(
-                        child: Text(
-                          titreLocal,
-                          style: const TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+      child: Image.network(
+        height:screenHeight * 0.2,
+       width :screenWidth,
+        imageLocal)
+    
     );
   }
 }
